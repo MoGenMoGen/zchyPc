254620f5b5eed28a0af34dd6d4b29083b1cc8a70
kdw <1197929159@qq.com>
2021-11-12 16:00:37 +0800

    整改单、云检验更换

--- components/person/index4.vue
+++ components/person/index4.vue
@@ -7,77 +7,8 @@
       @toClose="toClose"
       :applyInfo="applyInfo"
     ></download>
-    <!--整改单-->
-    <div class="message">
-      <div class="title">
-        <p>
-          <img
-            src="@/assets/img/personal/首页/整改单图标.png"
-            alt=""
-          />整改单<span class="more" @click="toPage('./rectification')"
-            >查看更多<i class="el-icon-arrow-right"></i
-          ></span>
-        </p>
-      </div>
-      <div class="content1">
-        <el-table
-          :data="rectifyList"
-          :cell-style="status"
-          style="width: 100%"
-          @row-click="toRectifydetail"
-        >
-          <el-table-column
-            type="index"
-            :index="indexMethod"
-            align="center"
-            width="60"
-            label="序号"
-          >
-          </el-table-column>
-          <el-table-column prop="cd" width="150" label="整改单号">
-          </el-table-column>
-          <el-table-column
-            width="200"
-            prop="nm"
-            align="center"
-            label="整改内容"
-          >
-          </el-table-column>
-          <el-table-column
-            width="110"
-            prop="issueTm"
-            align="center"
-            label="下发日期"
-          >
-          </el-table-column>
-          <el-table-column
-            prop="status"
-            fixed="right"
-            width="110"
-            align="center"
-            label="当前状态"
-          >
-            <template slot-scope="scope">
-              <div
-                v-if="scope.row.state == 2"
-                style="color: #ff3c00; border: 1px solid #ff3c00"
-              >
-                待执行
-              </div>
-              <div
-                v-else-if="scope.row.state == 3"
-                style="color: #2778be; border: 1px solid #2778be"
-              >
-                待结案
-              </div>
-              <div v-else-if="scope.row.state == 4">已完成</div>
-            </template>
-          </el-table-column>
-        </el-table>
-      </div>
-    </div>
     <!--云检验-->
-    <div class="manage">
+    <div class="message">
       <div class="title">
         <p>
           <img
@@ -172,34 +103,138 @@
         </el-table>
       </div>
     </div>
+    <!--整改单-->
+    <div class="manage">
+      <div class="title">
+        <p>
+          <img
+            src="@/assets/img/personal/首页/整改单图标.png"
+            alt=""
+          />整改单<span class="more" @click="toPage('./rectification')"
+            >查看更多<i class="el-icon-arrow-right"></i
+          ></span>
+        </p>
+      </div>
+      <div class="content1">
+        <el-table
+          :data="rectifyList"
+          :cell-style="status"
+          style="width: 100%"
+          @row-click="toRectifydetail"
+        >
+          <el-table-column
+            type="index"
+            :index="indexMethod"
+            align="center"
+            width="60"
+            label="序号"
+          >
+          </el-table-column>
+          <el-table-column prop="cd" width="150" label="整改单号">
+          </el-table-column>
+          <el-table-column
+            width="200"
+            prop="nm"
+            align="center"
+            label="整改内容"
+          >
+          </el-table-column>
+          <el-table-column
+            width="110"
+            prop="issueTm"
+            align="center"
+            label="下发日期"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="status"
+            fixed="right"
+            width="110"
+            align="center"
+            label="当前状态"
+          >
+            <template slot-scope="scope">
+              <div
+                v-if="scope.row.state == 2"
+                style="color: #ff3c00; border: 1px solid #ff3c00"
+              >
+                待执行
+              </div>
+              <div
+                v-else-if="scope.row.state == 3"
+                style="color: #2778be; border: 1px solid #2778be"
+              >
+                待结案
+              </div>
+              <div v-else-if="scope.row.state == 4">已完成</div>
+            </template>
+          </el-table-column>
+        </el-table>
+      </div>
+    </div>
+
     <!--投标管理-->
     <div class="manage">
       <div class="title">
-        <p><img src="@/assets/img/personal/首页/投标管理.png" alt=""/>投标管理<span class="more" @click="toPage('./tender?cdType=1')">查看更多<i class="el-icon-arrow-right"></i></span></p>
+        <p>
+          <img
+            src="@/assets/img/personal/首页/投标管理.png"
+            alt=""
+          />投标管理<span class="more" @click="toPage('./tender?cdType=1')"
+            >查看更多<i class="el-icon-arrow-right"></i
+          ></span>
+        </p>
       </div>
       <div class="content1">
         <el-table :data="bidList" style="width: 100%">
-          <el-table-column type="index" align="center" min-width="150" label="序号">
+          <el-table-column
+            type="index"
+            align="center"
+            min-width="150"
+            label="序号"
+          >
           </el-table-column>
           <el-table-column align="center" min-width="200" label="项目信息">
             <template slot-scope="scope">
-              <p>{{scope.row.nm}}</p>
-              <p>项目编号：{{scope.row.cd}}</p>
+              <p>{{ scope.row.nm }}</p>
+              <p>项目编号：{{ scope.row.cd }}</p>
             </template>
           </el-table-column>
           <el-table-column width="80" prop="budget" align="center" label="采购金额">
+
           </el-table-column>
-          <el-table-column width="170" prop="bidOpenTm" align="center" label="开标时间">
+          <el-table-column
+            width="170"
+            prop="bidOpenTm"
+            align="center"
+            label="开标时间"
+          >
           </el-table-column>
           <el-table-column width="110" align="center" label="状态">
             <template slot-scope="scope">
-              <p v-if="scope.row.bidDecideTm" style="color: #E4393C;">已定标</p>
-              <p v-else-if="returnDate(1,scope.row.bidOpenTm)" style="color: #2778BE;">评标中</p>
-              <p v-else-if="returnDate(2,scope.row.bidEndTm)" style="color: #3FAB2E;">投标中</p>
-              <p v-else style="color: #E4393C;">待开标</p>
+              <p v-if="scope.row.bidDecideTm" style="color: #e4393c">已定标</p>
+              <p
+                v-else-if="returnDate(1, scope.row.bidOpenTm)"
+                style="color: #2778be"
+              >
+                评标中
+              </p>
+              <p
+                v-else-if="returnDate(2, scope.row.bidEndTm)"
+                style="color: #3fab2e"
+              >
+                投标中
+              </p>
+              <p v-else style="color: #e4393c">待开标</p>
             </template>
           </el-table-column>
-          <el-table-column align="center" width="110" fixed="right" prop="operations" label="操作">
+          <el-table-column
+            align="center"
+            width="110"
+            fixed="right"
+            prop="operations"
+            label="操作"
+          >
             <div class="btnList" slot-scope="scope">
               <button class="button3" v-if="scope.row.depositStatus==2" @click="openBail(scope.row)" style="font-size: 12px;">保证金上传</button>
               <button class="button3" v-if="(scope.row.depositStatus==1||scope.row.depositStatus==3)&&!scope.row.bidDecideTm&&returnDate(2,scope.row.bidEndTm)&&!scope.row.offer" @click="openOffer(scope.row)">投标报价</button>
@@ -275,13 +310,14 @@
 import { mapState } from "vuex";
 
 import Offer from "../../components/person/offer";
-  import bail from "../../components/person/bail";
+import bail from "../../components/person/bail";
 import Download from "@/components/person/download";
 export default {
   name: "shipowner",
   components: {
     Download,
-    Offer,bail
+    Offer,
+    bail,
   },
   data() {
     return {
@@ -289,9 +325,9 @@ export default {
       bail: false,
       download1: false,
       applyInfo: {},
-      identityCd: '',
-      currentRoleId: '',
-      nowDate: '',
+      identityCd: "",
+      currentRoleId: "",
+      nowDate: "",
       // 整改列表
       rectifyList: [],
 
@@ -345,35 +381,34 @@ export default {
   computed: {
     ...mapState(["currentRole"]),
     returnDate() {
-      return (type,date) => {
-        if(type==1) {
-          if((new Date(date)).getTime()<this.nowDate){
-            return true
+      return (type, date) => {
+        if (type == 1) {
+          if (new Date(date).getTime() < this.nowDate) {
+            return true;
           } else {
-            return false
+            return false;
           }
-        } else if(type==2) {
-          if((new Date(date)).getTime()>this.nowDate){
-            return true
+        } else if (type == 2) {
+          if (new Date(date).getTime() > this.nowDate) {
+            return true;
           } else {
-            return false
+            return false;
           }
         }
-
-      }
-    }
+      };
+    },
   },
   watch: {
     currentRole() {
-      this.identityCd = JSON.parse(this.until.seGet('currentRole')).identityCd
-      this.currentRoleId = JSON.parse(this.until.seGet('currentRole')).id
+      this.identityCd = JSON.parse(this.until.seGet("currentRole")).identityCd;
+      this.currentRoleId = JSON.parse(this.until.seGet("currentRole")).id;
       this.getBidData();
     },
   },
   mounted() {
-    this.nowDate = (new Date()).getTime()
-    this.identityCd = JSON.parse(this.until.seGet('currentRole')).identityCd
-    this.currentRoleId = JSON.parse(this.until.seGet('currentRole')).id
+    this.nowDate = new Date().getTime();
+    this.identityCd = JSON.parse(this.until.seGet("currentRole")).identityCd;
+    this.currentRoleId = JSON.parse(this.until.seGet("currentRole")).id;
     this.getData();
     // 获取整改单列表
     this.getrectifyList();
@@ -443,11 +478,11 @@ export default {
     //投标详情
     toDetail(row) {
       this.$router.push({
-        path: './bidAfficheDetail',
+        path: "./bidAfficheDetail",
         query: {
           id: row.bidId,
-        }
-      })
+        },
+      });
     },
     statusYun({ row, column, rowIndex, columnIndex }) {
       if (columnIndex == 0) {
@@ -484,36 +519,38 @@ export default {
       return index + 1;
     },
     openBail(row) {
-      this.bail = true
-      this.applyInfo = row
+      this.bail = true;
+      this.applyInfo = row;
     },
     openOffer(row) {
-        this.offer = true
-        this.applyInfo = row
+      this.offer = true;
+      this.applyInfo = row;
     },
     close() {
       this.offer = false;
-      this.getBidData()
+      this.getBidData();
     },
     close2() {
       this.bail = false;
-      this.getBidData()
+      this.getBidData();
     },
     getBidData() {
-      let qry = this.query.new()
-      this.query.toO(qry, 'publishTm', 'desc')
-      this.query.toP(qry, 1, 3)
+      let qry = this.query.new();
+      this.query.toO(qry, "publishTm", "desc");
+      this.query.toP(qry, 1, 3);
       // this.query.toW(qry, 'viewRangeCd', this.identityCd+'', 'LK')
-      this.api.getMyBidList(this.query.toEncode(qry),this.currentRoleId).then(res => {
-        this.bidList = res.data.list
-        this.total = res.page.total
-      })
+      this.api
+        .getMyBidList(this.query.toEncode(qry), this.currentRoleId)
+        .then((res) => {
+          this.bidList = res.data.list;
+          this.total = res.page.total;
+        });
     },
     sign(row) {
-      let id = row.signin.shipBidSigninVo.id
-      this.api.bidSign(id).then(res => {
-        this.$message.success('签到成功')
-      })
+      let id = row.signin.shipBidSigninVo.id;
+      this.api.bidSign(id).then((res) => {
+        this.$message.success("签到成功");
+      });
     },
     toRectifydetail(row) {
       this.$router.push(`./rectificationDetail?id=${row.id}`);
