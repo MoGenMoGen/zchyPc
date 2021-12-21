03ec8207daa7cf37ddb6e75275c120b89acf9d36
yf <1060292708@qq.com>
2021-12-21 23:44:10 +0800

    修改问题

--- pages/personal/msgDtl.vue
+++ pages/personal/msgDtl.vue
@@ -1,89 +1,102 @@
 <template>
-<!--  消息详情-->
-    <div class="main">
-      <div style="padding:0 30px">
-        <tobbar :title="title" :showAll="false"></tobbar>
-      </div>
-      <p style="border-bottom: 1px solid rgba(0,0,0,0.1);"></p>
-
-      <div class="line4">
-        <p>{{info.nm}}</p>
-        <p>{{info.sendTm}}</p>
-        <div>
-          <p v-html="info.cont"></p>
-          <span @click="toDetail" v-if="info.targetUrl">查看详情</span>
-        </div>
-      </div>
-
-
-
+  <!--  消息详情-->
+  <div class="main">
+    <div style="padding:0 30px">
+      <tobbar :title="title" :showAll="false"></tobbar>
     </div>
+    <p style="border-bottom: 1px solid rgba(0,0,0,0.1);"></p>
+
+    <div class="line4">
+      <p>{{info.nm}}</p>
+      <p>{{info.sendTm}}</p>
+      <div>
+        <p v-html="info.cont"></p>
+        <span @click="toDetail" v-if="info.targetUrl">查看详情</span>
+      </div>
+    </div>
+
+
+
+  </div>
 </template>
 
 <script>
-    import tobbar from "../../components/person/tobbar";
-    import searchIc from '../../assets/img/personal/search.png'
-    import deleteIc from '../../assets/img/personal/delete.png'
-    import successIc from '../../assets/img/success.png'
-    export default {
-        name: "allShip",
-        components:{
-            tobbar,
-        },
-        mounted(){
-            let id=this.$route.query.id
-            let that = this
-             this.api.msgDetail(id).then(res=>{
-               that.info=res
-               that.api.msgUnRead()
-             })
-            },
-        methods:{
-          toDetail(){
-            if(this.info.targetUrl.indexOf('/personal/bid')!=-1) {
-              this.$message.error('该消息已撤销');
-            } else {
-              window.location.href = this.info.targetUrl
-            }
-          },
+  import tobbar from "../../components/person/tobbar";
+  import searchIc from '../../assets/img/personal/search.png'
+  import deleteIc from '../../assets/img/personal/delete.png'
+  import successIc from '../../assets/img/success.png'
+  export default {
+    name: "allShip",
+    components: {
+      tobbar,
+    },
+    mounted() {
+      let id = this.$route.query.id
+      let that = this
+      this.api.msgDetail(id).then(res => {
+        that.info = res
+        that.api.msgUnRead()
+      })
+    },
+    methods: {
+      toDetail() {
+        if (this.info.targetUrl.indexOf('/personal/bid') != -1) {
+          this.$message.error('该消息已撤销');
+        } else {
+          window.location.href = this.info.targetUrl
+        }
+      },
 
-        },
-        data(){
-            return{
-                title:'消息详情',
-                searchIc,
-                deleteIc,
-                successIc,
-                value: '',
-                info:{},
+    },
+    data() {
+      return {
+        title: '消息详情',
+        searchIc,
+        deleteIc,
+        successIc,
+        value: '',
+        info: {},
 
 
-            }
-        },
-        layout:'person',
+      }
+    },
+    layout: 'person',
 
-    }
+  }
 </script>
 
 <style scoped lang="less">
   @import url("../../assets/css/init.less");
-  .main{
+
+  .main {
     width: 100%;
 
 
-    .line4{
-     span{
-       text-decoration: underline;
-       color: @themeColor;
-     }
+    .line4 {
+      span {
+        text-decoration: underline;
+        color: @themeColor;
+      }
+
       margin-top: 20px;
-      p:nth-of-type(1){
+
+      p:nth-of-type(1) {
         text-align: center;
         color: #333333;
         font-size: 20px;
       }
-      p:nth-of-type(2){
+
+      p:nth-of-type(2) {
         margin-top: 10px;
         margin-bottom: 20px;
         text-align: center;
         color: #999999;
+      }
+
+      div {
+        padding: 0 40px;
+      }
+
+    }
+  }
+</style>
